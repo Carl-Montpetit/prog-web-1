77598e46dd6d980e1a81c54e020978d687a66b4e
Matthew <jovani.matthew@courrier.uqam.ca>
2021-06-14 01:38:55 -0400

    Fixed Login Popup

--- JS/Main.js
+++ JS/Main.js
@@ -1,8 +1,30 @@
+createForm = document.getElementById("createAccountForm");
+loginButton = document.getElementById("monCompte");
+loggedInButton = document.getElementById("nomLogin");
+
+
+
 function openForm() {
-  window.open(href="CreateAccount.html", "createAccount", "width=500,height=400");
+  createForm.style.display = "inline";
 }
 
-function navLogin() {
-  var login = document.getElementById("monCompte");
-  login.style.display = "none";
+function validateCreateAccount(pw, pw2) {
+  if (pw === pw2) {
+    createForm.style.display = "none";
+    loginButton.style.display = "none";
+    userNaame = String(document.getElementById("prenom").value) + ' <i class="fa fa-child" ></i>';
+    document.getElementById("userName").innerHTML = userNaame;
+    loggedInButton.style.display = "inline-block";
+  } else {
+      invalidPw();
+  }
+}
+
+function invalidPw() {
+  var alert = document.getElementById("pwDoesNotMatch");
+  alert.style.display = "inline";
+}
+
+function getUserName() {
+  return userName;
 }
