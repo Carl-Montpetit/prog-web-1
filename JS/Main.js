1408bf2ecb994a91d0e53da431699671280b80f5
Matthew <jovani.matthew@courrier.uqam.ca>
2021-06-15 00:28:48 -0400

    Added universal Navbar

--- JS/Main.js
+++ JS/Main.js
@@ -1,19 +1,40 @@
-createForm = document.getElementById("createAccountForm");
-loginButton = document.getElementById("monCompte");
-loggedInButton = document.getElementById("nomLogin");
-
-
-
 function openForm() {
+  createForm = document.getElementById("createAccountForm");
   createForm.style.display = "inline";
+}
+
+function connect() {
+  connectForm = document.getElementById("connectForm");
+  connectForm.style.display = "inline";
+}
+
+function disconnect() {
+  loggedInButton = document.getElementById("nomLogin");
+  loginButton = document.getElementById("monCompte");
+  loggedInButton.style.display = "none";
+  loginButton.style.display = "inline-block";
+}
+
+function validateAccount() {
+  connectForm = document.getElementById("connectForm");
+  loginButton = document.getElementById("monCompte");
+  loggedInButton = document.getElementById("nomLogin");
+  connectForm.style.display = "none";
+  loginButton.style.display = "none";
+  userName = String(document.getElementById("prenom").value) + ' <i class="fa fa-child" ></i>';
+  document.getElementById("userName").innerHTML = userName;
+  loggedInButton.style.display = "inline-block";
 }
 
 function validateCreateAccount(pw, pw2) {
   if (pw === pw2) {
+    createForm = document.getElementById("createAccountForm");
+    loginButton = document.getElementById("monCompte");
+    loggedInButton = document.getElementById("nomLogin");
     createForm.style.display = "none";
     loginButton.style.display = "none";
-    userNaame = String(document.getElementById("prenom").value) + ' <i class="fa fa-child" ></i>';
-    document.getElementById("userName").innerHTML = userNaame;
+    userName = String(document.getElementById("prenom").value) + ' <i class="fa fa-child" ></i>';
+    document.getElementById("userName").innerHTML = userName;
     loggedInButton.style.display = "inline-block";
   } else {
       invalidPw();
@@ -23,8 +44,4 @@ function validateCreateAccount(pw, pw2) {
 function invalidPw() {
   var alert = document.getElementById("pwDoesNotMatch");
   alert.style.display = "inline";
-}
-
-function getUserName() {
-  return userName;
 }
