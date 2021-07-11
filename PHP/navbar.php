311f13435c6e4d35a5e392f4139b4ddd3794b165
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-07-11 16:02:42 -0400

    ajout du php dans nav bar pour le create account

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -1,7 +1,22 @@
-<link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="../CSS/navbar.css">
-<script src="../JS/Main.js"></script>
+<!DOCTYPE html>
+
+<html lang="eng">
+<head>
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <script src="../JS/Main.js"></script>
+</head>
+<?php
+function test_input( $data )
+{
+    $data = trim( $data );
+    $data = stripslashes( $data );
+    $data = htmlspecialchars( $data );
+    return $data;
+}
+
+?>
 <body>
 <nav>
     <div class="dropdown">
@@ -41,12 +56,12 @@
 </nav>
 
 <div id="createAccountForm">
-    <form id="createForm">
+    <form id="createForm" method="post" action="<?php echo $_SERVER[ 'PHP_SELF' ]; ?>">
         <br>
         <label for="text">Prenom:</label><br>
-        <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prenom" required><br>
+        <input type="text" id="prenom" name="first_name" placeholder="Entrez votre prenom"><br>
         <label for="text">Nom:</label><br>
-        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom" required><br>
+        <input type="text" id="nom" name="last_name" placeholder="Entrez votre nom" required><br>
         <label for="email">Courriel:</label><br>
         <input type="email" id="email" name="email" placeholder="Entrez votre courriel" required><br>
         <label for="password">Mot de passe:</label><br>
@@ -56,6 +71,43 @@
         <br>
         <input type="submit" value="Enregistrer">
     </form>
+    <?php
+    // define variables and set to empty values
+    $first_name_err = $last_name_err = $email_err = $pw1_err = $pw2_err = "";
+    $first_name = $last_name = $email = $pw1 = $pw2 = "";
+
+    if ( $_SERVER[ "REQUEST_METHOD" ] == "POST" ) {
+        if ( empty( $_POST[ "first_name" ] ) ) {
+            $first_name_err = "Le prénom est requis!";
+        } else {
+            $first_name = test_input( $_POST[ "first_name" ] );
+        }
+
+        if ( empty( $_POST[ "last_name" ] ) ) {
+            $last_name_err = "Le nom est requis!";
+        } else {
+            $last_name = test_input( $_POST[ "last_name" ] );
+        }
+
+        if ( empty( $_POST[ "email" ] ) ) {
+            $email_err = "Le courriel est requis!";
+        } else {
+            $email = test_input( $_POST[ "email" ] );
+        }
+
+        if ( empty( $_POST[ "pw1" ] ) ) {
+            $pw1_err = "Le mot de passe est requis!";
+        } else {
+            $pw1 = test_input( $_POST[ "pw1" ] );
+        }
+
+        if ( empty( $_POST[ "pw2" ] ) ) {
+            $pw2_err = "La vérification du mot de passe est requise!";
+        } else {
+            $pw2 = test_input( $_POST[ "pw2" ] );
+        }
+    }
+    ?>
 </div>
 
 <div id="connectAccountForm">
@@ -70,3 +122,6 @@
         <input type="submit" value="Se Connecter">
     </form>
 </div>
+
+</body>
+</html>
