037c0d15611f6ca985c2509f34542c2245216f0b
Matthew <jovani.matthew@courrier.uqam.ca>
2021-07-13 02:09:30 -0400

    Ajouté une option pour que l'Admin cree des programmes

--- JS/Main.js
+++ JS/Main.js
@@ -91,4 +91,54 @@ function drop( ev ) {
         item.childNodes[ 7 ].remove();
     }
     item.style.height = "20px";
+    cartText = document.getElementById("cartText");
+    cartText.style.display = 'none';
+}
+
+function addProgram() {
+  var td = document.createElement('td');
+  document.getElementById("addNewPrograms").appendChild(td);
+  section = document.createElement('section');
+  section.setAttribute('class', 'relativeObjects');
+  section.setAttribute('draggable', 'true');
+  section.setAttribute('ondragstart', 'drag(event)');
+  td.appendChild(section);
+  button = document.createElement('button');
+  button.setAttribute('class','plusButton');
+  button.setAttribute('onclick','namePrompt()');
+  section.appendChild(button);
+  var plus = document.createElement('i');
+  plus.setAttribute('class','fa fa-plus');
+  button.appendChild(plus);
+}
+
+function namePrompt() {
+  var programName = prompt("Nom du programme");
+  if(programName != null) {
+    document.getElementsByClassName('plusButton')[0].removeAttribute('onclick');
+    var h2 = document.createElement('h2');
+    h2Text = document.createTextNode(programName);
+    h2.appendChild(h2Text);
+    section.appendChild(h2);
+    h2.parentNode.insertBefore(h2, h2.previousElementSibling);
+    button.setAttribute('onclick','pricePrompt()');
+  }
+}
+
+function pricePrompt() {
+  var programPrice = prompt("Le prix du programme");
+  if(programPrice != null) {
+    document.getElementsByClassName('plusButton')[0].removeAttribute('onclick');
+    var h3 = document.createElement('h3');
+    h3Text = document.createTextNode(programPrice);
+    h3.appendChild(h3Text);
+    section.appendChild(h3);
+    h3.parentNode.insertBefore(h3, h3.previousElementSibling);
+    var h4 = document.createElement('h4');
+    h4Text = document.createTextNode("Ce programme comprend :");
+    h4.appendChild(h4Text);
+    section.appendChild(h4);
+    h4.parentNode.insertBefore(h4, h4.previousElementSibling);
+    //button.setAttribute('onclick','BulletPrompt()');
+  }
 }
--- PHP/Programs.php
+++ PHP/Programs.php
@@ -3,93 +3,95 @@
 
 <head>
     <meta charset="UTF-8">
-    <!-- Ajust the display in function of the device -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- for icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- To link the file to the style sheet -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../CSS/Programs.css">
-
     <title>Programmes</title>
 </head>
 
 <body>
-<iframe src="./navbar.php" frameborder="0"></iframe>
-<header>
-    <h1>Liste des programmes offert</h1>
-</header>
-<table>
-    <td>
-        <section draggable="true" ondragstart="drag(event)" id="classic">
-            <h2><strong>LE CLASSIQUE</strong></h2>
-            <h3><strong>100$</strong></h3>
-            <h4>Ce programme comprend :</h4>
-            <ul>
-                <li>Un bloc d'activités sportif</li>
-                <li>
-                    Un autre bloc d’activités possédant :
-                </li>
+    <iframe id="navBar" onmouseover="unRelative()" src="./navbar.php" frameborder="0"></iframe>
+    <button id="createProgram" onclick="addProgram()">+ Creer un nouveau program</button>  <!--TODO: make this button available for admin ONLY  -->
+    
+    <header>
+        <h1>Liste des programmes offert</h1>
+    </header>
+    <table>
+        <td>
+            <section class="relativeObjects" draggable="true" ondragstart="drag(event)" id="classic">
+                <h2>Le Classique</strong></h2>
+                <h3>100$</strong></h3>
+                <h4>Ce programme comprend :</h4>
                 <ul>
-                    <li>Une activité de type <i>art</i></li>
-                    <li>Une activité de type <i>science</i></li>
+                    <li>Un bloc d'activités sportif</li>
+                    <li>
+                        Un autre bloc d’activités possédant :
+                    </li>
+                    <ul>
+                        <li>Une activité de type <i>art</i> </li>
+                        <li>Une activité de type <i>science</i></li>
+                    </ul>
                 </ul>
-            </ul>
-        </section>
-    </td>
-    <td>
-        <section draggable="true" ondragstart="drag(event)" id="artscience">
-            <h2><strong>LES ARTS ET LA SCIENCE</strong></h2>
-            <h3><strong>150$</strong></h3>
-            <h4>Ce programme comprend :</h4>
-            <ul>
-                <li>Plusieurs activités du type :</li>
+            </section>
+        </td>
+        <td>
+            <section class="relativeObjects" draggable="true" ondragstart="drag(event)" id="artscience">
+                <h2>Les Arts et La Science</strong></h2>
+                <h3>150$</strong></h3>
+                <h4>Ce programme comprend :</h4>
                 <ul>
-                    <li>Culinaires</li>
-                    <li>Arts visuels</li>
-                    <li>Arts plastiques</li>
-                    <li>Chimie</li>
-                    <li>Biologie</li>
-                    <li>Physique</li>
+                    <li>Plusieurs activités du type :</li>
+                    <ul>
+                        <li>Culinaires</li>
+                        <li>Arts visuels</li>
+                        <li>Arts plastiques</li>
+                        <li>Chimie</li>
+                        <li>Biologie</li>
+                        <li>Physique</li>
+                    </ul>
                 </ul>
-            </ul>
-            <ul>
-                <li>Une activité matinale est réservé pour pratiquer :</li>
                 <ul>
-                    <li>Le yoga</li>
-                    <li>Jeu de course comme le ballon chasseur</li>
+                    <li>Une activité matinale est réservé pour pratiquer :</li>
+                    <ul>
+                        <li>Le yoga</li>
+                        <li>Jeu de course comme le ballon chasseur</li>
+                    </ul>
                 </ul>
-            </ul>
-            <h4>
-                Ce programme <u>ne comprend pas</u><br><br><br> d'activités du type <i>sportif</i> !
-            </h4>
-        </section>
-    </td>
-    <td>
-        <section draggable="true" ondragstart="drag(event)" id="sport">
-            <h2><strong>L'ENFANT ACTIF</strong></h2>
-            <h3><strong>100$</strong></h3>
-            <h4>Ce programme comprend :</h4>
-            <ul>
-                <li>Au moins 4 activités quotidiennes parmi :
-                </li>
+                <h4>
+                    Ce programme <u>ne comprend pas</u><br><br><br> d'activités du type <i>sportif</i> !
+                </h4>
+            </section>
+        </td>
+        <td>
+            <section class="relativeObjects" draggable="true" ondragstart="drag(event)" id="sport">
+                <h2>L'enfant Actif</strong></h2>
+                <h3>100$</strong></h3>
+                <h4>Ce programme comprend :</h4>
                 <ul>
-                    <li>Le basketball</li>
-                    <li>Le tennis</li>
-                    <li>Le soccer</li>
-                    <li>Le ballon chasseur</li>
-                    <li>Le baseball</li>
-                    <li>Et plus encore!</li>
+                    <li>Au moins 4 activités quotidiennes parmi :
+                    </li>
+                    <ul>
+                        <li>Le basketball</li>
+                        <li>Le tennis</li>
+                        <li>Le soccer</li>
+                        <li>Le ballon chasseur</li>
+                        <li>Le baseball</li>
+                        <li>Et plus encore!</li>
+                    </ul>
                 </ul>
-            </ul>
-        </section>
-    </td>
-    <td>
-        <section ondrop="drop(event)" ondragover="allowDrop(event)" id="cart"><i class="fa fa-cart-arrow-down"></i>
-        </section>
-    </td>
-</table>
+            </section>
+        </td>
+        <td id="addNewPrograms"></td>
+            <section class="relativeObjects" ondrop="drop(event)" ondragover="allowDrop(event)" id="cart">
+                <i class="fa fa-cart-arrow-down"></i>
+                <span id="cartText"><br>Glissez vos programs<br>
+             dans le panier</span>
+            </section>
+    </table>
 
-<script src="../JS/Main.js"></script>
+    <script src="../JS/Main.js"></script>
 </body>
 
 </html>
