e3fea2aa2c2de9af5f2c7d74dc0d3b1a1a1ccbf9
Massoud Ferdoussi <ferdoussi.massoud@courrier.uqam.ca>
2021-07-16 03:32:51 -0400

    Changed login.php w/o DB and fix cancel btn

--- JS/Login.js
+++ JS/Login.js
@@ -1,25 +1,25 @@
-function connect(formConnect) {
-    let form = document.getElementById( formConnect );
+function connect() {
+    form = document.getElementById("connectForm");
     form.onsubmit = function () {
-        if(form.elements.value != "") {
+        if (form.elements.value != "") {
             let value = form.first_name.value;
             var iframe = document.getElementById('navbar');
-            var anonymousObjects = iframe.contentWindow.document.getElementsByClassName( "anonymous" );
+            var anonymousObjects = iframe.contentWindow.document.getElementsByClassName("anonymous");
             var i;
-            for ( i = 0; i < anonymousObjects.length; i++ ) {
-                anonymousObjects[ i ].style.display = "none";
+            for (i = 0; i < anonymousObjects.length; i++) {
+                anonymousObjects[i].style.display = "none";
             }
-            var loggedInObjects = iframe.contentWindow.document.getElementsByClassName( "loggedIn" );
-            for ( i = 0; i < loggedInObjects.length; i++ ) {
-                loggedInObjects[ i ].style.display = "inline-block";
+            var loggedInObjects = iframe.contentWindow.document.getElementsByClassName("loggedIn");
+            for (i = 0; i < loggedInObjects.length; i++) {
+                loggedInObjects[i].style.display = "inline-block";
             }
             value = value + ' <i class="fa fa-child" ></i>';
-            iframe.contentWindow.document.getElementById( "monCompteButton" ).innerHTML = value;
+            iframe.contentWindow.document.getElementById("monCompteButton").innerHTML = value;
             return false;
         }
     };
     form.onreset = function () {
         location.replace('../PHP/Main.php');
     }
-    
+
 }
--- PHP/CreateAccount.php
+++ PHP/CreateAccount.php
@@ -1,50 +1,3 @@
-<?php 
-
-include 'config.php';
-
-error_reporting(0);
-
-session_start();
-
-if (isset($_SESSION['email'])) {
-    header("Location: Login.php");
-}
-
-if (isset($_POST['submit'])) {
-	$firstname = $_POST['first_name'];
-	$lastname = $_POST['last_name'];
-	$email = $_POST['email'];
-	$password = md5($_POST['pw1']);
-	$cpassword = md5($_POST['pw2']);
-
-	if ($password == $cpassword) {
-		$sql = "SELECT * FROM users WHERE email='$email'";
-		$result = mysqli_query($conn, $sql);
-		if (!$result->num_rows > 0) {
-			$sql = "INSERT INTO users (firstname, lastname, email, password)
-					VALUES ('$firstname', '$lastname', '$email', '$password')";
-			$result = mysqli_query($conn, $sql);
-			if ($result) {
-				echo "<script>alert('Enregistrement de l'utilisateur terminé!')</script>";
-				$firstname = "";
-				$lastname = "";
-				$email = "";
-				$_POST['password'] = "";
-				$_POST['cpassword'] = "";
-			} else {
-				echo "<script>alert('Erreur!')</script>";
-			}
-		} else {
-			echo "<script>alert('L'email existe déjà.')</script>";
-		}
-		
-	} else {
-		echo "<script>alert('Le mot de passe ne correspond pas.')</script>";
-	}
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -58,7 +11,7 @@ if (isset($_POST['submit'])) {
 
         <title>Créer un Compte</title>
     </head>
-    <body>
+    <body onload="connect()">
         <header>
             <!-- The navbar -->
             <iframe id="navbar" src="./navbar.php" frameborder="0"></iframe>
@@ -70,17 +23,19 @@ if (isset($_POST['submit'])) {
             <form id="inputs_createAccount" method="post" action="">
                 <br>
                 <label for="text">Prénom:</label><br>
-                <input type="text" id="first_name" name="first_name" placeholder="Entrez votre prénom" value="<?php echo $firstname; ?>" required>
+                <input type="text" id="first_name" name="first_name" placeholder="Entrez votre prénom" required>
                 <br>
                 <label for="text">Nom:</label><br>
-                <input type="text" id="last_name" name="last_name" placeholder="Entrez votre nom" value="<?php echo $lastname; ?>" required><br>
+                <input type="text" id="last_name" name="last_name" placeholder="Entrez votre nom" required><br>
                 <label for="email">Courriel:</label><br>
-                <input type="email" id="email" name="email" placeholder="Entrez votre courriel" value="<?php echo $email; ?>" required><br>
+                <input type="email" id="email" name="email" placeholder="Entrez votre courriel" required><br>
                 <label for="password">Mot de passe:</label><br>
-                <input type="password" id="pw1" name="pw1" placeholder="Créer un mot de passe" value="<?php echo $_POST['password']; ?>" required><br>
-                <input type="password" id="pw2" name="pw2" placeholder="Confirmez le mot de passe" value="<?php echo $_POST['cpassword']; ?>" required><br>
+                <input type="password" id="pw1" name="pw1" placeholder="Créer un mot de passe" required><br>
+                <input type="password" id="pw2" name="pw2" placeholder="Confirmez le mot de passe" required><br>
                 <input id="cancel" class="button" type="reset" value="Annuler">
                 <input class="button" type="submit" name = "submit" value="Enregistrer">
         </div>
+    <!-- JS -->
+    <script src="../JS/CreateAccount.js"></script>
     </body>
 </html>
